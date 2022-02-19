21ca18788d723df9086136db2ecb5062209a59d4
Siddheshwar Kurhe <58392823+skurhe@users.noreply.github.com>
2022-02-19 18:29:40 +0530

    Add files via upload

---
+++ Que1/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Que1.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public decimal Rate { get; set; }
+        public string Description { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
